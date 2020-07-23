222bf0d7b0eace437dad685209d1784f018233b9
Maximilian Kroker <kroker@not-a.lawyer>
2020-07-23 18:09:02 +0200

    database done

--- database.py
+++ database.py
@@ -3,4 +3,17 @@ import psycopg2 as psycopg2
 
 def connect_data():
 
-    conn = psycopg2.connect("dbname=suppliers user= password=")
+    conn = psycopg2.connect("dbname=capacity")
+    cursor = conn.cursor()
+    return conn, cursor
+
+def create_table(cursor, command):
+    cursor.execute(command)
+
+def add_data_to_db(data):
+    conn, cursor = connect_data()
+    for i in range(len(data)):
+        capacity = data[i][0]
+        district = data[i][1]
+        cursor.execute("INSERT INTO entries (district_name, capacity) VALUES (%s, %s)", (district, capacity))
+    conn.commit()
--- test.py
+++ test.py
@@ -8,5 +8,20 @@ class MyTestCase(unittest.TestCase):
     def test_getting_site_content(self):
         soup = crawl_url("https://member.superfit.club/CheckinCounter/GetClubsCheckinCounterPage")
         result = process_soup(soup)
-        connect_data()
+        add_data_to_db(result)
+
+
         pass
+    def test_database_connection(self):
+        connection, cursor = connect_data()
+        create_table(cursor,
+        """CREATE TABLE entries (
+            entry_id SERIAL PRIMARY KEY,
+            district_name VARCHAR(255) NOT NULL,
+            capacity INTEGER NOT NULL,
+            time TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0) 
+        )"""
+        )
+        cursor.close()
+        connection.commit()
+        connection.close()
